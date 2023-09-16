bb3c32ccb2fb38ada256c0aaa6ad4cce6f6681f3
roxidev <rashadulalam7767@gmail.com>
2023-09-15 20:07:44 -0700

    set the remaining credit

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -1,21 +1,29 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-key */
 //  eslint-disable react/jsx-key
-const Card = ({ selectedCourses, remainingCreadit }) => {
+const Card = ({
+  selectedCourses,
+  remainingCreadit,
+  totalPrice,
+  totalCradit,
+}) => {
   // console.log(selectedCourses);
   return (
     <div>
       <h3 className="text-sm font-bold text-[#2F80ED]">
         Credit Hour Remaining {remainingCreadit} hr
       </h3>
+      <hr />
       <h2 className="text-xl font-bold text-[#1C1B1B]">Course Name</h2>
-      {selectedCourses.map((course, index) => (
-        <ol>
-          <li key={selectedCourses}>
-            {index + 1}-{course.course_name}
-          </li>
-        </ol>
+      {selectedCourses.map((course) => (
+        <li className="list-decimal" key={course.id}>
+          {course.course_name}
+        </li>
       ))}
+      <hr />
+      <h2>Total Credit Hour:{totalCradit}</h2>
+      <hr />
+      <h2>Total Price:{totalPrice}</h2>
     </div>
   );
 };
--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,11 +1,21 @@
 /* eslint-disable react/jsx-key */
+import { FiDollarSign } from 'react-icons/fi';
+import { HiOutlineBookOpen } from 'react-icons/hi';
 import { useEffect, useState } from 'react';
 import Card from '../Card/Card';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Home = () => {
   const [allCourse, setAllCourse] = useState([]);
+
   const [selectedCourses, setSelectedCourses] = useState([]);
+
   const [remainingCreadit, setRemainingCreadit] = useState(20);
+  // setRemainingCreadit(10);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalCradit, setTotalCradit] = useState(0);
+
   // console.log(selectedCourses);
 
   useEffect(() => {
@@ -13,32 +23,48 @@ const Home = () => {
       .then((res) => res.json())
       .then((data) => setAllCourse(data));
   });
-
+  const handleCradit = (course) => {
+    const newRemainingCreadit = remainingCreadit + course.credit;
+    setRemainingCreadit(newRemainingCreadit);
+  };
   const handleSelectCourse = (course) => {
+    const newAllcourse = [...selectedCourses, course];
+    const newTotalPrice = totalPrice + course.price;
+    const newTotalCreadit = totalCradit + course.credit;
+
     const newRemaining = remainingCreadit - course.credit;
 
     const isExist = selectedCourses.find((item) => item.id == course.id);
-
-    if (isExist) {
-      alert('Already join this course!!');
-    } else {
-      setSelectedCourses([...selectedCourses, course]);
-      setRemainingCreadit(newRemaining);
+    let newRemainingTotalCreaditHours = 0;
+    if (remainingCreadit < course.credit) {
+      return toast.warning('You have not supetient creadit');
     }
 
-    // console.log(selectedCourses);
+    if (isExist) {
+      toast.warn('Course already added');
+    } else {
+      toast.success('Successfully course added');
+      setSelectedCourses(newAllcourse);
+
+      setRemainingCreadit(newRemaining);
+      setTotalPrice(newTotalPrice);
+      setTotalCradit(newTotalCreadit);
+    }
   };
 
   return (
-    <div className="flex gap-6 mx-4">
-      <div className="w-2/3 ">
+    <div className="flex gap-6 mx-14 ">
+      <div className="w-3/4 bg-[#F3F3F3]">
         <h1 className="text-2xl text-[#1C1B1B] font-bold mt-12 mb-8 text-center">
           Course Registration
         </h1>
 
-        <div className="grid grid-cols-3  gap-6 ">
+        <div className="grid grid-cols-3  gap-6 bg-[#F3F3F3]">
           {allCourse.map((course) => (
-            <div key={course.id} className=" mt-4 bg-[#fff] rounded-xl">
+            <div
+              key={course.id}
+              className=" mt-4 bg-[#fff] rounded-xl py-4 px-4"
+            >
               <figure className="">
                 <img src={course.img} alt="Shoes" className="rounded-xl" />
               </figure>
@@ -49,36 +75,44 @@ const Home = () => {
                 <p className="text-xs text-[#1C1B1B99] font-normal ">
                   {course.description}
                 </p>
-                <div className="flex justify-between">
-                  <div className="mt-5 mb-6">
-                    <img src="" alt="" />
-                    <p className="text-xs text-[#1C1B1B99] font-medium">
+                <div className="flex justify-between mb-6 mt-5">
+                  <div className="  flex">
+                    <FiDollarSign className="text-xl" />
+                    <span className="text-xs text-[#1C1B1B99] font-medium">
                       price: {course.price}
-                    </p>
+                    </span>
                   </div>
-                  <div className="">
-                    <img src="" alt="" />
-                    <p className="text-xs text-[#1C1B1B99] font-medium">
+                  <div className="flex">
+                    <HiOutlineBookOpen
+                      onClick={() => handleCradit(course)}
+                      className="text-xl"
+                    />
+                    <span className="text-xs text-[#1C1B1B99] font-medium">
                       credit:{course.credit}h
-                    </p>
+                    </span>
                   </div>
                 </div>
                 <div
-                  onClick={() => handleSelectCourse(course)}
                   className="border-2 border-solid border-[#2F80ED] bg-[#2F80ED] mx-auto text-center rounded-lg py-2"
+                  onClick={() => handleSelectCourse(course)}
                 >
                   <button>Select</button>
                 </div>
+                <ToastContainer />
               </div>
             </div>
           ))}
         </div>
       </div>
-      <div className="w-1/3 mt-28 bg-slate-400">
-        <Card
-          remainingCreadit={remainingCreadit}
-          selectedCourses={selectedCourses}
-        />
+      <div className="w-1/4 mt-32 bg-[#F3F3F3]">
+        <div className="bg-[#fff] py-4 px-4 rounded-xl">
+          <Card
+            remainingCreadit={remainingCreadit}
+            selectedCourses={selectedCourses}
+            totalPrice={totalPrice}
+            totalCradit={totalCradit}
+          />
+        </div>
       </div>
     </div>
   );
